aa134bb62ebae397e4429dab3724894abc2cef30
Vitalii Ryzhenko <vr@Vitaliis-MacBook-Pro.local>
2023-07-18 09:53:01 +0200

    +data.json, task:layout and style:5

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
-const Tree = () => {
+import treeData from "./data.json";
+
+const TreeNode = ({ node }) => {
+	const { label, key, children } = node;
 	return (
-		<div className='tree'>
-			root
+		<>
+			<span>{key}</span><span>{label}</span>
 			<br />
 			<ul>
-				ant <br />
-				bear
-				<ul>
-					cat <br />
-					dog
-					<ul>elephant</ul>
-				</ul>
-				frog
+				{!!children.length &&
+					children.map((childNode) => (
+						<TreeNode
+							key={childNode.key}
+							node={childNode}
+						/>
+					))}
 			</ul>
+		</>
+	);
+};
+
+const Tree = () => {
+	const [tree, setTree] = useState(treeData.tree);
+	return (
+		<div className='tree'>
+			{tree.map((node) => (
+				<TreeNode
+					key={node.key}
+					node={node}
+				/>
+			))}
 		</div>
 	);
 };
